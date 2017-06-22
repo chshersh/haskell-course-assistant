d23cc94c5ba1f0f3eae7b06bf2a0a758a403bf57
Dmitry Kovanikov <kovanikov@gmail.com>
2017-06-22 22:26:30 +0300

    Bump up LTS and update runner

--- runner.sh
+++ runner.sh
@@ -1,1 +1,1 @@
-stack exec assistant -- 0 ~/programming/haskell/cw/tm/students ~/programming/haskell/cw/tm/tm28 ~/programming/haskell/cw/tm/gen14
+stack exec assistant -- 0 ~/programming/haskell/teaching/exams/tmv/students ~/programming/haskell/teaching/exams/tmv/tm23 ~/programming/haskell/teaching/exams/tmv/gen23
